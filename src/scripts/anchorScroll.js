1d0c7b4110a7fb819e0f145a6430e07824794f19
evmajoar <evmajoar@gmail.com>
2019-07-13 00:05:27 +0300

    Обновление

--- src/scripts/anchorScroll.js
+++ src/scripts/anchorScroll.js
@@ -7,9 +7,9 @@ if ( $anchor.length ) {
         var $that = $( this ), hb = $('body, html');
 
         if ( $that.is( 'button' ) ) {
-            hb.stop().animate({ scrollTop: $( '#' + $that.data( 'id' ) ).offset().top }, 1000, 'swing');
+            hb.stop().animate({ scrollTop: $( '.' + $that.data( 'id' ) ).offset().top }, 1000, 'swing');
         } else if ( $that.is( 'a' ) ) {
-            hb.stop().animate({ scrollTop: $( $that.attr( 'href' ) ).offset().top }, 1000, 'swing');
+            hb.stop().animate({ scrollTop: $( '.' + $that.attr( 'href' ).replace('#', '') ).offset().top }, 1000, 'swing');
         }
 
     });
